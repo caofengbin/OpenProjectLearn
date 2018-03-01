686a3a1532d198029906e987f9ff8ac7a57c5f5d
caofengbin <1050430934@qq.com>
2018-03-01 20:39:10 +0800

    add comments about diapatcher

--- app/src/main/java/okhttp3/Dispatcher.java
+++ app/src/main/java/okhttp3/Dispatcher.java
@@ -33,197 +33,226 @@ import okhttp3.internal.Util;
 
 /**
  * Policy on when async requests are executed.
- *
+ * <p>
  * <p>Each dispatcher uses an {@link ExecutorService} to run calls internally. If you supply your
  * own executor, it should be able to run {@linkplain #getMaxRequests the configured maximum} number
  * of calls concurrently.
  */
 public final class Dispatcher {
-  private int maxRequests = 64;
-  private int maxRequestsPerHost = 5;
-  private @Nullable
-  Runnable idleCallback;
+    private int maxRequests = 64;
+    private int maxRequestsPerHost = 5;
+    private
+    @Nullable
+    Runnable idleCallback;
 
-  /** Executes calls. Created lazily. */
-  private @Nullable ExecutorService executorService;
+    /**
+     * Executes calls. Created lazily.
+     */
+    private
+    @Nullable
+    ExecutorService executorService;
 
-  /** Ready async calls in the order they'll be run. */
-  private final Deque<AsyncCall> readyAsyncCalls = new ArrayDeque<>();
+    /**
+     * 就绪状态的异步请求队列
+     * Ready async calls in the order they'll be run.
+     */
+    private final Deque<AsyncCall> readyAsyncCalls = new ArrayDeque<>();
 
-  /** Running asynchronous calls. Includes canceled calls that haven't finished yet. */
-  private final Deque<AsyncCall> runningAsyncCalls = new ArrayDeque<>();
+    /**
+     * 正在执行的异步请求队列
+     * Running asynchronous calls. Includes canceled calls that haven't finished yet.
+     */
+    private final Deque<AsyncCall> runningAsyncCalls = new ArrayDeque<>();
 
-  /** Running synchronous calls. Includes canceled calls that haven't finished yet. */
-  private final Deque<RealCall> runningSyncCalls = new ArrayDeque<>();
+    /**
+     * Running synchronous calls. Includes canceled calls that haven't finished yet.
+     */
+    private final Deque<RealCall> runningSyncCalls = new ArrayDeque<>();
 
-  public Dispatcher(ExecutorService executorService) {
-    this.executorService = executorService;
-  }
-
-  public Dispatcher() {
-  }
-
-  /**
-   * 一个单例创建线程池
-   * @return
-   */
-  public synchronized ExecutorService executorService() {
-    if (executorService == null) {
-      /**
-       * 设定Integer.MAX_VALUE对整个的行为影响不大，
-       * 通过队列控制上线为64
-       */
-      executorService = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60, TimeUnit.SECONDS,
-          new SynchronousQueue<Runnable>(), Util.threadFactory("OkHttp Dispatcher", false));
+    public Dispatcher(ExecutorService executorService) {
+        this.executorService = executorService;
     }
-    return executorService;
-  }
 
-  /**
-   * Set the maximum number of requests to execute concurrently. Above this requests queue in
-   * memory, waiting for the running calls to complete.
-   *
-   * <p>If more than {@code maxRequests} requests are in flight when this is invoked, those requests
-   * will remain in flight.
-   */
-  public synchronized void setMaxRequests(int maxRequests) {
-    if (maxRequests < 1) {
-      throw new IllegalArgumentException("max < 1: " + maxRequests);
+    public Dispatcher() {
     }
-    this.maxRequests = maxRequests;
-    promoteCalls();
-  }
 
-  public synchronized int getMaxRequests() {
-    return maxRequests;
-  }
-
-  /**
-   * Set the maximum number of requests for each host to execute concurrently. This limits requests
-   * by the URL's host name. Note that concurrent requests to a single IP address may still exceed
-   * this limit: multiple hostnames may share an IP address or be routed through the same HTTP
-   * proxy.
-   *
-   * <p>If more than {@code maxRequestsPerHost} requests are in flight when this is invoked, those
-   * requests will remain in flight.
-   *
-   * <p>WebSocket connections to hosts <b>do not</b> count against this limit.
-   */
-  public synchronized void setMaxRequestsPerHost(int maxRequestsPerHost) {
-    if (maxRequestsPerHost < 1) {
-      throw new IllegalArgumentException("max < 1: " + maxRequestsPerHost);
+    /**
+     * 一个单例创建线程池
+     *
+     * @return
+     */
+    public synchronized ExecutorService executorService() {
+        if (executorService == null) {
+            /**
+             * 设定Integer.MAX_VALUE对整个的行为影响不大，
+             * 通过队列控制上线为64
+             */
+            executorService = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60, TimeUnit.SECONDS,
+                    new SynchronousQueue<Runnable>(), Util.threadFactory("OkHttp Dispatcher", false));
+        }
+        return executorService;
     }
-    this.maxRequestsPerHost = maxRequestsPerHost;
-    promoteCalls();
-  }
 
-  public synchronized int getMaxRequestsPerHost() {
-    return maxRequestsPerHost;
-  }
+    /**
+     * Set the maximum number of requests to execute concurrently. Above this requests queue in
+     * memory, waiting for the running calls to complete.
+     * <p>
+     * <p>If more than {@code maxRequests} requests are in flight when this is invoked, those requests
+     * will remain in flight.
+     */
+    public synchronized void setMaxRequests(int maxRequests) {
+        if (maxRequests < 1) {
+            throw new IllegalArgumentException("max < 1: " + maxRequests);
+        }
+        this.maxRequests = maxRequests;
+        promoteCalls();
+    }
 
-  /**
-   * Set a callback to be invoked each time the dispatcher becomes idle (when the number of running
-   * calls returns to zero).
-   *
-   * <p>Note: The time at which a {@linkplain Call call} is considered idle is different depending
-   * on whether it was run {@linkplain Call#enqueue(Callback) asynchronously} or
-   * {@linkplain Call#execute() synchronously}. Asynchronous calls become idle after the
-   * {@link Callback#onResponse onResponse} or {@link Callback#onFailure onFailure} callback has
-   * returned. Synchronous calls become idle once {@link Call#execute() execute()} returns. This
-   * means that if you are doing synchronous calls the network layer will not truly be idle until
-   * every returned {@link Response} has been closed.
-   */
-  public synchronized void setIdleCallback(@Nullable Runnable idleCallback) {
-    this.idleCallback = idleCallback;
-  }
+    public synchronized int getMaxRequests() {
+        return maxRequests;
+    }
 
+    /**
+     * Set the maximum number of requests for each host to execute concurrently. This limits requests
+     * by the URL's host name. Note that concurrent requests to a single IP address may still exceed
+     * this limit: multiple hostnames may share an IP address or be routed through the same HTTP
+     * proxy.
+     * <p>
+     * <p>If more than {@code maxRequestsPerHost} requests are in flight when this is invoked, those
+     * requests will remain in flight.
+     * <p>
+     * <p>WebSocket connections to hosts <b>do not</b> count against this limit.
+     */
+    public synchronized void setMaxRequestsPerHost(int maxRequestsPerHost) {
+        if (maxRequestsPerHost < 1) {
+            throw new IllegalArgumentException("max < 1: " + maxRequestsPerHost);
+        }
+        this.maxRequestsPerHost = maxRequestsPerHost;
+        promoteCalls();
+    }
+
+    public synchronized int getMaxRequestsPerHost() {
+        return maxRequestsPerHost;
+    }
+
+    /**
+     * Set a callback to be invoked each time the dispatcher becomes idle (when the number of running
+     * calls returns to zero).
+     * <p>
+     * <p>Note: The time at which a {@linkplain Call call} is considered idle is different depending
+     * on whether it was run {@linkplain Call#enqueue(Callback) asynchronously} or
+     * {@linkplain Call#execute() synchronously}. Asynchronous calls become idle after the
+     * {@link Callback#onResponse onResponse} or {@link Callback#onFailure onFailure} callback has
+     * returned. Synchronous calls become idle once {@link Call#execute() execute()} returns. This
+     * means that if you are doing synchronous calls the network layer will not truly be idle until
+     * every returned {@link Response} has been closed.
+     */
+    public synchronized void setIdleCallback(@Nullable Runnable idleCallback) {
+        this.idleCallback = idleCallback;
+    }
+
+    /**
+     * 异步请求通过dispatcher做的操作
+     *
+     * @param call 异步请求call
+     */
     synchronized void enqueue(AsyncCall call) {
         if (runningAsyncCalls.size() < maxRequests && runningCallsForHost(call) < maxRequestsPerHost) {
+            // 符合立即执行的条件
             runningAsyncCalls.add(call);
             executorService().execute(call);
         } else {
+            // 进行缓存等待的操作
             readyAsyncCalls.add(call);
         }
     }
 
-  /**
-   * Cancel all calls currently enqueued or executing. Includes calls executed both {@linkplain
-   * Call#execute() synchronously} and {@linkplain Call#enqueue asynchronously}.
-   */
-  public synchronized void cancelAll() {
-    for (AsyncCall call : readyAsyncCalls) {
-      call.get().cancel();
+    /**
+     * Cancel all calls currently enqueued or executing. Includes calls executed both {@linkplain
+     * Call#execute() synchronously} and {@linkplain Call#enqueue asynchronously}.
+     */
+    public synchronized void cancelAll() {
+        for (AsyncCall call : readyAsyncCalls) {
+            call.get().cancel();
+        }
+
+        for (AsyncCall call : runningAsyncCalls) {
+            call.get().cancel();
+        }
+
+        for (RealCall call : runningSyncCalls) {
+            call.cancel();
+        }
     }
 
-    for (AsyncCall call : runningAsyncCalls) {
-      call.get().cancel();
+    /**
+     * 调整异步请求的队列
+     */
+    private void promoteCalls() {
+        if (runningAsyncCalls.size() >= maxRequests) return; // Already running max capacity.
+        if (readyAsyncCalls.isEmpty()) return; // No ready calls to promote.
+
+        // 对等待的异步请求队列进行调度
+        for (Iterator<AsyncCall> i = readyAsyncCalls.iterator(); i.hasNext(); ) {
+            AsyncCall call = i.next();
+
+            if (runningCallsForHost(call) < maxRequestsPerHost) {
+                i.remove();
+                runningAsyncCalls.add(call);
+                executorService().execute(call);
+            }
+
+            if (runningAsyncCalls.size() >= maxRequests) return; // Reached max capacity.
+        }
     }
 
-    for (RealCall call : runningSyncCalls) {
-      call.cancel();
+    /**
+     * Returns the number of running calls that share a host with {@code call}.
+     * 判断当前正在执行的网络请求中，与当前的AsyncCall属于同一个host的网络请求的数量
+     */
+    private int runningCallsForHost(AsyncCall call) {
+        int result = 0;
+        for (AsyncCall c : runningAsyncCalls) {
+            if (c.get().forWebSocket) continue;
+            if (c.host().equals(call.host())) result++;
+        }
+        return result;
     }
-  }
 
-  /**
-   * 调整异步请求的队列
-   */
-  private void promoteCalls() {
-    if (runningAsyncCalls.size() >= maxRequests) return; // Already running max capacity.
-    if (readyAsyncCalls.isEmpty()) return; // No ready calls to promote.
-
-    for (Iterator<AsyncCall> i = readyAsyncCalls.iterator(); i.hasNext(); ) {
-      AsyncCall call = i.next();
-
-      if (runningCallsForHost(call) < maxRequestsPerHost) {
-        i.remove();
-        runningAsyncCalls.add(call);
-        executorService().execute(call);
-      }
-
-      if (runningAsyncCalls.size() >= maxRequests) return; // Reached max capacity.
+    /**
+     * 同步请求通过dispatcher做的操作
+     * Used by {@code Call#execute} to signal it is in-flight.
+     */
+    synchronized void executed(RealCall call) {
+        runningSyncCalls.add(call);
     }
-  }
 
-  /**
-   * Returns the number of running calls that share a host with {@code call}.
-   * 判断当前正在执行的网络请求中，与当前的AsyncCall属于同一个host的网络请求的数量
-   * */
-  private int runningCallsForHost(AsyncCall call) {
-    int result = 0;
-    for (AsyncCall c : runningAsyncCalls) {
-      if (c.get().forWebSocket) continue;
-      if (c.host().equals(call.host())) result++;
+    /**
+     * Used by {@code AsyncCall#run} to signal completion.
+     */
+    void finished(AsyncCall call) {
+        finished(runningAsyncCalls, call, true);
     }
-    return result;
-  }
 
-  /** Used by {@code Call#execute} to signal it is in-flight. */
-  synchronized void executed(RealCall call) {
-    runningSyncCalls.add(call);
-  }
-
-  /** Used by {@code AsyncCall#run} to signal completion. */
-  void finished(AsyncCall call) {
-    finished(runningAsyncCalls, call, true);
-  }
-
-  /** Used by {@code Call#execute} to signal completion. */
-  void finished(RealCall call) {
-    finished(runningSyncCalls, call, false);
-  }
+    /**
+     * Used by {@code Call#execute} to signal completion.
+     */
+    void finished(RealCall call) {
+        finished(runningSyncCalls, call, false);
+    }
 
     private <T> void finished(Deque<T> calls, T call, boolean promoteCalls) {
         int runningCallsCount;
         Runnable idleCallback;
         synchronized (this) {
-            // 方法1：从runningSyncCalls队列中移除当前请求
+            // 方法1：从执行队列中移除当前请求
             if (!calls.remove(call)) {
-              throw new AssertionError("Call wasn't in-flight!");
+                throw new AssertionError("Call wasn't in-flight!");
             }
 
             // 方法2：调整整个异步请求任务队列
             if (promoteCalls) {
-              promoteCalls();
+                promoteCalls();
             }
 
             // 方法3：重新计算运行的线程的数量
@@ -236,30 +265,34 @@ public final class Dispatcher {
         }
     }
 
-  /** Returns a snapshot of the calls currently awaiting execution. */
-  public synchronized List<Call> queuedCalls() {
-    List<Call> result = new ArrayList<>();
-    for (AsyncCall asyncCall : readyAsyncCalls) {
-      result.add(asyncCall.get());
+    /**
+     * Returns a snapshot of the calls currently awaiting execution.
+     */
+    public synchronized List<Call> queuedCalls() {
+        List<Call> result = new ArrayList<>();
+        for (AsyncCall asyncCall : readyAsyncCalls) {
+            result.add(asyncCall.get());
+        }
+        return Collections.unmodifiableList(result);
     }
-    return Collections.unmodifiableList(result);
-  }
 
-  /** Returns a snapshot of the calls currently being executed. */
-  public synchronized List<Call> runningCalls() {
-    List<Call> result = new ArrayList<>();
-    result.addAll(runningSyncCalls);
-    for (AsyncCall asyncCall : runningAsyncCalls) {
-      result.add(asyncCall.get());
+    /**
+     * Returns a snapshot of the calls currently being executed.
+     */
+    public synchronized List<Call> runningCalls() {
+        List<Call> result = new ArrayList<>();
+        result.addAll(runningSyncCalls);
+        for (AsyncCall asyncCall : runningAsyncCalls) {
+            result.add(asyncCall.get());
+        }
+        return Collections.unmodifiableList(result);
     }
-    return Collections.unmodifiableList(result);
-  }
 
-  public synchronized int queuedCallsCount() {
-    return readyAsyncCalls.size();
-  }
+    public synchronized int queuedCallsCount() {
+        return readyAsyncCalls.size();
+    }
 
-  public synchronized int runningCallsCount() {
-    return runningAsyncCalls.size() + runningSyncCalls.size();
-  }
+    public synchronized int runningCallsCount() {
+        return runningAsyncCalls.size() + runningSyncCalls.size();
+    }
 }
--- app/src/main/java/okhttp3/RealCall.java
+++ app/src/main/java/okhttp3/RealCall.java
@@ -18,6 +18,7 @@ package okhttp3;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import okhttp3.internal.NamedRunnable;
 import okhttp3.internal.cache.CacheInterceptor;
 import okhttp3.internal.connection.ConnectInterceptor;
@@ -31,39 +32,42 @@ import okhttp3.internal.platform.Platform;
 import static okhttp3.internal.platform.Platform.INFO;
 
 final class RealCall implements Call {
-  final OkHttpClient client;
-  final RetryAndFollowUpInterceptor retryAndFollowUpInterceptor;
+    final OkHttpClient client;
+    final RetryAndFollowUpInterceptor retryAndFollowUpInterceptor;
 
-  /**
-   * There is a cycle between the {@link Call} and {@link EventListener} that makes this awkward.
-   * This will be set after we create the call instance then create the event listener instance.
-   */
-  private EventListener eventListener;
+    /**
+     * There is a cycle between the {@link Call} and {@link EventListener} that makes this awkward.
+     * This will be set after we create the call instance then create the event listener instance.
+     */
+    private EventListener eventListener;
 
-  /** The application's original request unadulterated by redirects or auth headers. */
-  final Request originalRequest;
-  final boolean forWebSocket;
+    /**
+     * The application's original request unadulterated by redirects or auth headers.
+     */
+    final Request originalRequest;
+    final boolean forWebSocket;
 
-  // Guarded by this.
-  private boolean executed;
+    // Guarded by this.
+    private boolean executed;
 
-  private RealCall(OkHttpClient client, Request originalRequest, boolean forWebSocket) {
-    this.client = client;
-    this.originalRequest = originalRequest;
-    this.forWebSocket = forWebSocket;
-    this.retryAndFollowUpInterceptor = new RetryAndFollowUpInterceptor(client, forWebSocket);
-  }
+    private RealCall(OkHttpClient client, Request originalRequest, boolean forWebSocket) {
+        this.client = client;
+        this.originalRequest = originalRequest;
+        this.forWebSocket = forWebSocket;
+        this.retryAndFollowUpInterceptor = new RetryAndFollowUpInterceptor(client, forWebSocket);
+    }
 
-  static RealCall newRealCall(OkHttpClient client, Request originalRequest, boolean forWebSocket) {
-    // Safely publish the Call instance to the EventListener.
-    RealCall call = new RealCall(client, originalRequest, forWebSocket);
-    call.eventListener = client.eventListenerFactory().create(call);
-    return call;
-  }
+    static RealCall newRealCall(OkHttpClient client, Request originalRequest, boolean forWebSocket) {
+        // Safely publish the Call instance to the EventListener.
+        RealCall call = new RealCall(client, originalRequest, forWebSocket);
+        call.eventListener = client.eventListenerFactory().create(call);
+        return call;
+    }
 
-  @Override public Request request() {
-    return originalRequest;
-  }
+    @Override
+    public Request request() {
+        return originalRequest;
+    }
 
     @Override
     public Response execute() throws IOException {
@@ -87,11 +91,10 @@ final class RealCall implements Call {
         }
     }
 
-  private void captureCallStackTrace() {
-    Object callStackTrace = Platform.get().getStackTraceForCloseable("response.body().close()");
-    retryAndFollowUpInterceptor.setCallStackTrace(callStackTrace);
-  }
-
+    private void captureCallStackTrace() {
+        Object callStackTrace = Platform.get().getStackTraceForCloseable("response.body().close()");
+        retryAndFollowUpInterceptor.setCallStackTrace(callStackTrace);
+    }
 
 
     /**
@@ -108,109 +111,117 @@ final class RealCall implements Call {
         client.dispatcher().enqueue(new AsyncCall(responseCallback));
     }
 
-  @Override public void cancel() {
-    retryAndFollowUpInterceptor.cancel();
-  }
-
-  @Override public synchronized boolean isExecuted() {
-    return executed;
-  }
-
-  @Override public boolean isCanceled() {
-    return retryAndFollowUpInterceptor.isCanceled();
-  }
-
-  @SuppressWarnings("CloneDoesntCallSuperClone") // We are a final type & this saves clearing state.
-  @Override public RealCall clone() {
-    return RealCall.newRealCall(client, originalRequest, forWebSocket);
-  }
-
-  StreamAllocation streamAllocation() {
-    return retryAndFollowUpInterceptor.streamAllocation();
-  }
-
-  final class AsyncCall extends NamedRunnable {
-    private final Callback responseCallback;
-
-    AsyncCall(Callback responseCallback) {
-      super("OkHttp %s", redactedUrl());
-      this.responseCallback = responseCallback;
+    @Override
+    public void cancel() {
+        retryAndFollowUpInterceptor.cancel();
     }
 
-    String host() {
-      return originalRequest.url().host();
+    @Override
+    public synchronized boolean isExecuted() {
+        return executed;
     }
 
-    Request request() {
-      return originalRequest;
+    @Override
+    public boolean isCanceled() {
+        return retryAndFollowUpInterceptor.isCanceled();
     }
 
-    RealCall get() {
-      return RealCall.this;
+    @SuppressWarnings("CloneDoesntCallSuperClone")
+    // We are a final type & this saves clearing state.
+    @Override
+    public RealCall clone() {
+        return RealCall.newRealCall(client, originalRequest, forWebSocket);
+    }
+
+    StreamAllocation streamAllocation() {
+        return retryAndFollowUpInterceptor.streamAllocation();
     }
 
     /**
-     * 父类runnable中通过模板方法调用
+     * 异步请求任务被封装为AsyncCall
      */
-    @Override
-    protected void execute() {
-      boolean signalledCallback = false;
-      try {
-        // OkHttp的核心
-        Response response = getResponseWithInterceptorChain();
-        if (retryAndFollowUpInterceptor.isCanceled()) {
-          signalledCallback = true;
-          responseCallback.onFailure(RealCall.this, new IOException("Canceled"));
-        } else {
-          signalledCallback = true;
-          responseCallback.onResponse(RealCall.this, response);
+    final class AsyncCall extends NamedRunnable {
+        private final Callback responseCallback;
+
+        AsyncCall(Callback responseCallback) {
+            super("OkHttp %s", redactedUrl());
+            this.responseCallback = responseCallback;
         }
-      } catch (IOException e) {
-        if (signalledCallback) {
-          // Do not signal the callback twice!
-          Platform.get().log(INFO, "Callback failure for " + toLoggableString(), e);
-        } else {
-          eventListener.callFailed(RealCall.this, e);
-          responseCallback.onFailure(RealCall.this, e);
+
+        String host() {
+            return originalRequest.url().host();
         }
-      } finally {
-        // 很重要的一个方法
-        client.dispatcher().finished(this);
-      }
+
+        Request request() {
+            return originalRequest;
+        }
+
+        RealCall get() {
+            return RealCall.this;
+        }
+
+        /**
+         * 父类runnable中通过模板方法调用
+         */
+        @Override
+        protected void execute() {
+            boolean signalledCallback = false;
+            try {
+                // OkHttp的核心
+                Response response = getResponseWithInterceptorChain();
+                if (retryAndFollowUpInterceptor.isCanceled()) {
+                    signalledCallback = true;
+                    responseCallback.onFailure(RealCall.this, new IOException("Canceled"));
+                } else {
+                    signalledCallback = true;
+                    responseCallback.onResponse(RealCall.this, response);
+                }
+            } catch (IOException e) {
+                if (signalledCallback) {
+                    // Do not signal the callback twice!
+                    Platform.get().log(INFO, "Callback failure for " + toLoggableString(), e);
+                } else {
+                    eventListener.callFailed(RealCall.this, e);
+                    responseCallback.onFailure(RealCall.this, e);
+                }
+            } finally {
+                // 很重要的一个方法
+                client.dispatcher().finished(this);
+            }
+        }
     }
-  }
 
-  /**
-   * Returns a string that describes this call. Doesn't include a full URL as that might contain
-   * sensitive information.
-   */
-  String toLoggableString() {
-    return (isCanceled() ? "canceled " : "")
-        + (forWebSocket ? "web socket" : "call")
-        + " to " + redactedUrl();
-  }
-
-  String redactedUrl() {
-    return originalRequest.url().redact();
-  }
-
-  Response getResponseWithInterceptorChain() throws IOException {
-    // Build a full stack of interceptors.
-    List<Interceptor> interceptors = new ArrayList<>();
-    interceptors.addAll(client.interceptors());
-    interceptors.add(retryAndFollowUpInterceptor);
-    interceptors.add(new BridgeInterceptor(client.cookieJar()));
-    interceptors.add(new CacheInterceptor(client.internalCache()));
-    interceptors.add(new ConnectInterceptor(client));
-    if (!forWebSocket) {
-      interceptors.addAll(client.networkInterceptors());
+    /**
+     * Returns a string that describes this call. Doesn't include a full URL as that might contain
+     * sensitive information.
+     */
+    String toLoggableString() {
+        return (isCanceled() ? "canceled " : "")
+                + (forWebSocket ? "web socket" : "call")
+                + " to " + redactedUrl();
     }
-    interceptors.add(new CallServerInterceptor(forWebSocket));
 
-    Interceptor.Chain chain = new RealInterceptorChain(interceptors, null, null, null, 0,
-        originalRequest, this, eventListener, client.connectTimeoutMillis(),
-        client.readTimeoutMillis(), client.writeTimeoutMillis());
+    String redactedUrl() {
+        return originalRequest.url().redact();
+    }
 
-    return chain.proceed(originalRequest);
-  }
+    Response getResponseWithInterceptorChain() throws IOException {
+        // Build a full stack of interceptors.
+        List<Interceptor> interceptors = new ArrayList<>();
+        interceptors.addAll(client.interceptors());
+        interceptors.add(retryAndFollowUpInterceptor);
+        interceptors.add(new BridgeInterceptor(client.cookieJar()));
+        interceptors.add(new CacheInterceptor(client.internalCache()));
+        interceptors.add(new ConnectInterceptor(client));
+        if (!forWebSocket) {
+            interceptors.addAll(client.networkInterceptors());
+        }
+        interceptors.add(new CallServerInterceptor(forWebSocket));
+
+        Interceptor.Chain chain = new RealInterceptorChain(interceptors, null, null, null, 0,
+                originalRequest, this, eventListener, client.connectTimeoutMillis(),
+                client.readTimeoutMillis(), client.writeTimeoutMillis());
+
+        return chain.proceed(originalRequest);
+    }
 }
